4cef00a168c053e740d2bc2e738994cfcdecc4fc
SzKristof97 <47104780+SzKristof97@users.noreply.github.com>
2020-04-23 13:49:10 +0200

    Second Level - Better And Better
    I working on the water and fog.
    The second presentation video is orientatid of these things.

---
+++ UnityLevelDesigns/Assets/MyAssets/Scripts/IncreaseDetailDistance.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseDetailDistance : MonoBehaviour
+{
+
+    public float RenderDetailsDistance = 1000;
+    void Update()
+    {
+        Terrain.activeTerrain.detailObjectDistance = RenderDetailsDistance;
+    }
+}
---
+++ UnityLevelDesigns/Assets/MyAssets/Scripts/RotateSun.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSun : MonoBehaviour
+{
+    enum TIME
+    {
+        DAY,
+        AFTERNOON,
+        MORNING,
+        NIGHT
+    }
+
+    public float rotateSpeed = 1f;
+
+    [Header("Day/Night time settings")]
+    public float DayTimeStart = 0f;
+    public float AfterNoonTimeStart = 0f;
+    public float MorningTimeStart = 0f;
+    public float NightTimeStart = 0f;
+
+    [Header("Fog settings")]
+    public float DayFog = 3000f;
+    public float AfterNoonFog = 500f;
+    public float NightFog = 350f;
+
+    [Header("Fog Colors")]
+    public Color DayTimeFogColor = Color.white;
+    public Color AfterNoonFogColor = Color.gray;
+    public Color NightFogColor = Color.black;
+
+    [SerializeField]
+    private TIME _currentTime = TIME.AFTERNOON;
+    private float _currentFog = 5000f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.left * rotateSpeed * Time.deltaTime);
+
+        CheckTime();
+    }
+
+    private void CheckTime()
+    {
+        Debug.Log("Rotation: " + transform.eulerAngles);
+        if (transform.eulerAngles.x <= NightTimeStart)
+        {
+            _currentTime = TIME.NIGHT;
+        }else if (transform.eulerAngles.x <= AfterNoonTimeStart)
+        {
+            _currentTime = TIME.AFTERNOON;
+        }
+        else if(transform.eulerAngles.x <= DayTimeStart)
+        {
+            _currentTime = TIME.DAY;
+        }
+        else if (transform.eulerAngles.x <= MorningTimeStart)
+        {
+            _currentTime = TIME.MORNING;
+        }
+        else
+        {
+            //Nothing happend
+        }
+    }
+}
